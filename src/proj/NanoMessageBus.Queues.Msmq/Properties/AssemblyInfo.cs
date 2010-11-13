20cd78b0d0c2f7dd7a6a7bde82f448a49aa9eb9f
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-11-13 11:05:08 -0700

    Added MSMQ project and SQL SubscriptionStorage project.

---
+++ src/proj/NanoMessageBus.Queues.Msmq/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NanoMessageBus.Queues.Msmq")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("8ca4f1c2-26b3-4707-8d7e-fb5997cfd779")]
---
+++ src/proj/NanoMessageBus.SubscriptionStorage.SqlEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NanoMessageBus.SubscriptionStorage.SqlEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("357766a7-d925-4a0d-9c00-a49be9eb24be")]
