93298b05fefa5e2417546f823fe8e6a279f77649
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-11-16 19:27:32 -0700

    Added default NullLogger in case no logging method is specified.

--- src/proj/NanoMessageBus.Logging/LogFactory.cs
+++ src/proj/NanoMessageBus.Logging/LogFactory.cs
@@ -4,6 +4,12 @@ namespace NanoMessageBus.Logging
 
 	public static class LogFactory
 	{
+		static LogFactory()
+		{
+			var logger = new NullLogger();
+			BuildLogger = type => logger;
+		}
+
 		public static Func<Type, ILog> BuildLogger { get; set; }
 	}
 }
---
+++ src/proj/NanoMessageBus.Logging/NullLogger.cs
@@ -0,0 +1,24 @@
+﻿namespace NanoMessageBus.Logging
+{
+	internal class NullLogger : ILog
+	{
+		public void Verbose(string message, params object[] values)
+		{
+		}
+		public void Debug(string message, params object[] values)
+		{
+		}
+		public void Info(string message, params object[] values)
+		{
+		}
+		public void Warn(string message, params object[] values)
+		{
+		}
+		public void Error(string message, params object[] values)
+		{
+		}
+		public void Fatal(string message, params object[] values)
+		{
+		}
+	}
+}
