f1315ccc6f4c7e83fb69b1c8770e5116c4b56ec4
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-11-17 07:02:30 -0700

    Added default unit of work handler.

---
+++ src/proj/NanoMessageBus.Implementation/Core/TransactionScopeUnitOfWork.cs
@@ -0,0 +1,55 @@
+namespace NanoMessageBus.Core
+{
+	using System;
+	using System.Transactions;
+
+	public class TransactionScopeUnitOfWork : IHandleUnitOfWork
+	{
+		private readonly TransactionScope transaction;
+		private bool disposed;
+
+		public TransactionScopeUnitOfWork()
+			: this(IsolationLevel.ReadCommitted)
+		{
+		}
+
+		public TransactionScopeUnitOfWork(IsolationLevel level)
+			: this(new TransactionOptions { IsolationLevel = level })
+		{
+		}
+
+		public TransactionScopeUnitOfWork(TransactionOptions options)
+			: this(new TransactionScope(TransactionScopeOption.Required, options))
+		{
+		}
+
+		public TransactionScopeUnitOfWork(TransactionScope transaction)
+		{
+			this.transaction = transaction;
+		}
+
+		~TransactionScopeUnitOfWork()
+		{
+			this.Dispose(false);
+		}
+
+		public void Dispose()
+		{
+			GC.SuppressFinalize(this);
+			this.Dispose(true);
+		}
+		protected virtual void Dispose(bool disposing)
+		{
+			if (this.disposed || !disposing)
+				return;
+
+			this.disposed = true;
+			this.transaction.Dispose();
+		}
+
+		public virtual void Complete()
+		{
+			this.transaction.Complete();
+		}
+	}
+}
