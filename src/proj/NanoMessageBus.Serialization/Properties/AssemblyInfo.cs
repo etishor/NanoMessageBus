e5cd2a22e3231dcc05e3f05e38772cc32d6d71ba
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-11-17 05:23:21 -0700

    Moved serialization into its own project.

---
+++ src/proj/NanoMessageBus.Serialization/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NanoMessageBus.Serialization")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("eb6b5835-176d-407b-a2a4-cfe916eb4c36")]
