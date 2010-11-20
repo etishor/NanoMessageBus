b6c543fd5c36375759c70541996ea5a8266983fd
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-11-20 11:04:36 -0700

    Updated PhysicalMessage to facilitate better serialization.

--- src/proj/NanoMessageBus/PhysicalMessage.cs
+++ src/proj/NanoMessageBus/PhysicalMessage.cs
@@ -2,10 +2,33 @@ namespace NanoMessageBus
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Runtime.Serialization;
 
+	[DataContract]
 	[Serializable]
 	public class PhysicalMessage
 	{
+		[DataMember(EmitDefaultValue = false, IsRequired = false)]
+		private readonly Guid messageId;
+
+		[DataMember(EmitDefaultValue = false, IsRequired = false)]
+		private readonly Guid correlationId;
+
+		[DataMember(EmitDefaultValue = false, IsRequired = false)]
+		private readonly string returnAddress;
+
+		[DataMember(EmitDefaultValue = false, IsRequired = false)]
+		private readonly DateTime expiration;
+
+		[DataMember(EmitDefaultValue = false, IsRequired = false)]
+		private readonly bool durable;
+
+		[DataMember(EmitDefaultValue = false, IsRequired = false)]
+		private readonly IDictionary<string, string> headers;
+
+		[DataMember(EmitDefaultValue = false, IsRequired = false)]
+		private readonly ICollection<object> logicalMessages;
+
 		public PhysicalMessage(
 			Guid messageId,
 			Guid correlationId,
@@ -15,21 +38,42 @@ namespace NanoMessageBus
 			IDictionary<string, string> headers,
 			ICollection<object> logicalMessages)
 		{
-			this.MessageId = messageId;
-			this.CorrelationId = correlationId;
-			this.ReturnAddress = returnAddress;
-			this.Expiration = expiration;
-			this.Durable = durable;
-			this.Headers = headers;
-			this.LogicalMessages = logicalMessages;
+			this.messageId = messageId;
+			this.correlationId = correlationId;
+			this.returnAddress = returnAddress;
+			this.expiration = expiration;
+			this.durable = durable;
+			this.headers = headers;
+			this.logicalMessages = logicalMessages;
 		}
 
-		public Guid MessageId { get; private set; }
-		public Guid CorrelationId { get; private set; }
-		public string ReturnAddress { get; private set; }
-		public DateTime Expiration { get; private set; }
-		public bool Durable { get; private set; }
-		public IDictionary<string, string> Headers { get; private set; }
-		public ICollection<object> LogicalMessages { get; private set; }
+		public Guid MessageId
+		{
+			get { return this.messageId; }
+		}
+		public Guid CorrelationId
+		{
+			get { return this.correlationId; }
+		}
+		public string ReturnAddress
+		{
+			get { return this.returnAddress; }
+		}
+		public DateTime Expiration
+		{
+			get { return this.expiration; }
+		}
+		public bool Durable
+		{
+			get { return this.durable; }
+		}
+		public IDictionary<string, string> Headers
+		{
+			get { return this.headers; }
+		}
+		public ICollection<object> LogicalMessages
+		{
+			get { return this.logicalMessages; }
+		}
 	}
 }
