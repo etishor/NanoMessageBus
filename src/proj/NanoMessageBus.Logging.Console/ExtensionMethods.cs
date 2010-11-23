0e2a8169860e710fa56653b4b0074fc66054884a
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-11-22 21:14:20 -0700

    Formatted default diagnostic message output.

--- src/proj/NanoMessageBus.Logging.Console/ExtensionMethods.cs
+++ src/proj/NanoMessageBus.Logging.Console/ExtensionMethods.cs
@@ -6,7 +6,7 @@ namespace NanoMessageBus.Logging
 
 	internal static class ExtensionMethods
 	{
-		private const string MessageFormat = "{0} - {1} - {2} - {3}\r\n";
+		private const string MessageFormat = "{0:yyyyMMdd.HHmmss.ff} - {1} - {2} - {3}\r\n";
 
 		public static string FormatMessage(this string message, Type typeToLog, params object[] values)
 		{
@@ -14,9 +14,15 @@ namespace NanoMessageBus.Logging
 				CultureInfo.InvariantCulture,
 				MessageFormat,
 				DateTime.UtcNow,
-				Thread.CurrentThread.Name,
+				Thread.CurrentThread.GetName(),
 				typeToLog.FullName,
 				string.Format(CultureInfo.InvariantCulture, message, values));
+		}
+		private static string GetName(this Thread thread)
+		{
+			return !string.IsNullOrEmpty(thread.Name)
+				? thread.Name
+				: thread.ManagedThreadId.ToString(CultureInfo.InvariantCulture);
 		}
 	}
 }
--- src/proj/NanoMessageBus.Logging.Console/TraceLogAdapter.cs
+++ src/proj/NanoMessageBus.Logging.Console/TraceLogAdapter.cs
@@ -19,36 +19,36 @@ namespace NanoMessageBus.Logging
 
 		public void Verbose(string message, params object[] values)
 		{
-			this.DebugWindow(message, values);
+			this.DebugWindow("Verbose / ", message, values);
 		}
 		public void Debug(string message, params object[] values)
 		{
-			this.DebugWindow(message, values);
+			this.DebugWindow("Debug / ", message, values);
 		}
 		public void Info(string message, params object[] values)
 		{
-			this.TraceWindow(message, values);
+			this.TraceWindow("Info / ", message, values);
 		}
 		public void Warn(string message, params object[] values)
 		{
-			this.TraceWindow(message, values);
+			this.TraceWindow("Warn / ", message, values);
 		}
 		public void Error(string message, params object[] values)
 		{
-			this.TraceWindow(message, values);
+			this.TraceWindow("Error / ", message, values);
 		}
 		public void Fatal(string message, params object[] values)
 		{
-			this.TraceWindow(message, values);
+			this.TraceWindow("Fatal / ", message, values);
 		}
 
-		private void DebugWindow(string message, params object[] values)
+		private void DebugWindow(string category, string message, params object[] values)
 		{
-			System.Diagnostics.Debug.WriteLine(message.FormatMessage(this.typeToLog, values));
+			System.Diagnostics.Debug.WriteLine(category, message.FormatMessage(this.typeToLog, values));
 		}
-		private void TraceWindow(string message, params object[] values)
+		private void TraceWindow(string category, string message, params object[] values)
 		{
-			Trace.WriteLine(message.FormatMessage(this.typeToLog, values));
+			Trace.Write(category, message.FormatMessage(this.typeToLog, values));
 		}
 	}
 }
