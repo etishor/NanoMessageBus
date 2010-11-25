d3845c5911fcef6780ffc262a1312b3018c1cbea
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-11-25 06:28:34 -0700

    Added message handler to take subscription messages and add them to the subscription storage.

---
+++ src/proj/NanoMessageBus.MessageSubscriber.DefaultImplementation/SubscriptionMessageHandlers.cs
@@ -0,0 +1,26 @@
+namespace NanoMessageBus.MessageSubscriber
+{
+	using SubscriptionStorage;
+
+	public class SubscriptionMessageHandlers : IHandleMessages<SubscriptionRequestMessage>,
+		IHandleMessages<UnsubscribeRequestMessage>
+	{
+		private readonly IStoreSubscriptions storage;
+		private readonly string subscriberAddress;
+
+		public SubscriptionMessageHandlers(IStoreSubscriptions storage, IMessageContext context)
+		{
+			this.storage = storage;
+			this.subscriberAddress = context.CurrentMessage.ReturnAddress;
+		}
+
+		public void Handle(SubscriptionRequestMessage message)
+		{
+			this.storage.Subscribe(this.subscriberAddress, message.MessageTypes, message.Expiration);
+		}
+		public void Handle(UnsubscribeRequestMessage message)
+		{
+			this.storage.Unsubscribe(this.subscriberAddress, message.MessageTypes);
+		}
+	}
+}
