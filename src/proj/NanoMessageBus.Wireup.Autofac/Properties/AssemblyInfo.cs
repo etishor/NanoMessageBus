2733fc166dc57bfc40dba329fc0618918515ad20
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-11-26 13:04:30 -0700

    Added wire-up project for Autofac IoC container.

---
+++ src/proj/NanoMessageBus.Wireup.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NanoMessageBus.Wireup.Autofac")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("e3fdd6c7-271b-4d74-ab94-3ac579f7baaf")]
