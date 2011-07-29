2d925a8da6b13239c489b9a00ef8710fec22a51a
Iulian Margarintescu <etishor@gmail.com>
2011-07-29 12:23:34 +0300

    Add posibility to execute an action when a message is moved to error queue. Usefull for notifications.

--- src/proj/NanoMessageBus.Core.ContextRouter/PoisonMessageHandler.cs
+++ src/proj/NanoMessageBus.Core.ContextRouter/PoisonMessageHandler.cs
@@ -1,74 +1,80 @@
 namespace NanoMessageBus.Core
 {
-	using System;
-	using System.Collections.Generic;
-	using Endpoints;
-	using Logging;
+    using System;
+    using System.Collections.Generic;
+    using Endpoints;
+    using Logging;
 
-	public class PoisonMessageHandler : IHandlePoisonMessages
-	{
-		private static readonly ILog Log = LogFactory.BuildLogger(typeof(PoisonMessageHandler));
+    public class PoisonMessageHandler : IHandlePoisonMessages
+    {
+        private static readonly ILog Log = LogFactory.BuildLogger(typeof(PoisonMessageHandler));
         private readonly Uri PoisonMessageQueue = null; // null == set by configuration
-		private readonly IDictionary<Guid, int> messageFailures = new Dictionary<Guid, int>();
-		private readonly ISendToEndpoints poisonQueue;
-		private readonly int maxRetries;
+        private readonly IDictionary<Guid, int> messageFailures = new Dictionary<Guid, int>();
+        private readonly ISendToEndpoints poisonQueue;
+        private readonly int maxRetries;
+        private readonly Action<EnvelopeMessage> postHandlingAction;
 
-		public PoisonMessageHandler(ISendToEndpoints poisonQueue, Uri poisonMessageQueue, int maxRetries)
-		{
-			this.poisonQueue = poisonQueue;
-			this.maxRetries = maxRetries;
+        public PoisonMessageHandler(ISendToEndpoints poisonQueue, Uri poisonMessageQueue, int maxRetries, Action<EnvelopeMessage> postHandlingAction)
+        {
+            this.poisonQueue = poisonQueue;
+            this.maxRetries = maxRetries;
             this.PoisonMessageQueue = poisonMessageQueue;
-		}
+            this.postHandlingAction = postHandlingAction;
+        }
 
-		public virtual bool IsPoison(EnvelopeMessage message)
-		{
-			if (message == null)
-				return false;
+        public virtual bool IsPoison(EnvelopeMessage message)
+        {
+            if (message == null)
+                return false;
 
-			int retries;
-			this.messageFailures.TryGetValue(message.MessageId, out retries);
-			return retries >= this.maxRetries;
-		}
+            int retries;
+            this.messageFailures.TryGetValue(message.MessageId, out retries);
+            return retries >= this.maxRetries;
+        }
 
-		public virtual void ClearFailures(EnvelopeMessage message)
-		{
-			if (message != null)
-				this.messageFailures.Remove(message.MessageId);
-		}
+        public virtual void ClearFailures(EnvelopeMessage message)
+        {
+            if (message != null)
+                this.messageFailures.Remove(message.MessageId);
+        }
 
-		public virtual void HandleFailure(EnvelopeMessage message, Exception exception)
-		{
-			if (this.CanRetry(message))
-				return;
+        public virtual void HandleFailure(EnvelopeMessage message, Exception exception)
+        {
+            if (this.CanRetry(message))
+                return;
 
-			AppendExceptionHeaders(message, exception, 0);
-			this.ForwardToPoisonMessageQueue(message);
-		}
-		private bool CanRetry(EnvelopeMessage message)
-		{
-			lock (this.messageFailures)
-			{
-				int retries;
-				this.messageFailures.TryGetValue(message.MessageId, out retries);
-				this.messageFailures[message.MessageId] = ++retries;
-				return retries < this.maxRetries;
-			}
-		}
-		private static void AppendExceptionHeaders(EnvelopeMessage message, Exception exception, int depth)
-		{
-			if (null == exception)
-				return;
+            AppendExceptionHeaders(message, exception, 0);
+            this.ForwardToPoisonMessageQueue(message);
+        }
+        private bool CanRetry(EnvelopeMessage message)
+        {
+            lock (this.messageFailures)
+            {
+                int retries;
+                this.messageFailures.TryGetValue(message.MessageId, out retries);
+                this.messageFailures[message.MessageId] = ++retries;
+                return retries < this.maxRetries;
+            }
+        }
+        private static void AppendExceptionHeaders(EnvelopeMessage message, Exception exception, int depth)
+        {
+            if (null == exception)
+                return;
 
-			message.Headers["ExceptionMessage" + depth] = exception.Message;
-			message.Headers["ExceptionStackTrace" + depth] = exception.StackTrace;
-			message.Headers["ExceptionSource" + depth] = exception.Source;
+            message.Headers["ExceptionMessage" + depth] = exception.Message;
+            message.Headers["ExceptionStackTrace" + depth] = exception.StackTrace;
+            message.Headers["ExceptionSource" + depth] = exception.Source;
 
-			AppendExceptionHeaders(message, exception.InnerException, ++depth);
-		}
-		private void ForwardToPoisonMessageQueue(EnvelopeMessage message)
-		{
-			Log.Info(Diagnostics.ForwardingMessageToPoisonMessageQueue, this.maxRetries, message.MessageId);
-            this.poisonQueue.Send(message, PoisonMessageQueue);	
-		}
-	}
+            AppendExceptionHeaders(message, exception.InnerException, ++depth);
+        }
+        private void ForwardToPoisonMessageQueue(EnvelopeMessage message)
+        {
+            Log.Info(Diagnostics.ForwardingMessageToPoisonMessageQueue, this.maxRetries, message.MessageId);
+            this.poisonQueue.Send(message, PoisonMessageQueue);
+            if (postHandlingAction != null)
+            {
+                postHandlingAction(message);
+            }
+        }
+    }
 }
--- src/proj/NanoMessageBus.Wireup.Autofac/EndpointWireup.cs
+++ src/proj/NanoMessageBus.Wireup.Autofac/EndpointWireup.cs
@@ -1,98 +1,106 @@
 namespace NanoMessageBus.Wireup
 {
-	using Autofac;
-	using Autofac.Core;
-	using Core;
-	using Endpoints;
-	using Serialization;
     using System;
+    using Autofac;
+    using Autofac.Core;
+    using Core;
+    using Endpoints;
+    using Serialization;
 
-	public class EndpointWireup : WireupModule
-	{
-		private const string PoisonEndpoint = "PoisonEndpoint";
-		private const bool PoisonEndpointEnlist = false;
-		private string receiverAddress;
-		private string poisonAddress;
-		private int maxRetries = 3;
-		private bool enlist = true;
+    public class EndpointWireup : WireupModule
+    {
+        private const string PoisonEndpoint = "PoisonEndpoint";
+        private const bool PoisonEndpointEnlist = false;
+        private string receiverAddress;
+        private string poisonAddress;
+        private int maxRetries = 3;
+        private bool enlist = true;
+        private Action<EnvelopeMessage> postHandlingAction;
 
-		public EndpointWireup(IWireup parent)
-			: base(parent)
-		{
-		}
+        public EndpointWireup(IWireup parent)
+            : base(parent)
+        {
+        }
 
-		public virtual EndpointWireup ListenOn(string address)
-		{
-			this.receiverAddress = address;
-			return this;
-		}
-		public virtual EndpointWireup ForwardPoisonMessagesTo(string address)
-		{
-			this.poisonAddress = address;
-			return this;
-		}
-		public virtual EndpointWireup RetryAtLeast(int times)
-		{
-			this.maxRetries = times;
-			return this;
-		}
-		public virtual EndpointWireup IgnoreTransactions()
-		{
-			this.enlist = false;
-			return this;
-		}
+        public virtual EndpointWireup ListenOn(string address)
+        {
+            this.receiverAddress = address;
+            return this;
+        }
+        public virtual EndpointWireup ForwardPoisonMessagesTo(string address)
+        {
+            this.poisonAddress = address;
+            return this;
+        }
 
-		protected override void Load(ContainerBuilder builder)
-		{
-			builder
-				.Register(this.BuildReceiverEndpoint)
-				.As<IReceiveFromEndpoints>()
-				.SingleInstance();
+        public virtual EndpointWireup PostPoisonMessageHandlingAction(Action<EnvelopeMessage> postHandlingAction)
+        {
+            this.postHandlingAction = postHandlingAction;
+            return this;
+        }
 
-			builder
-				.Register(this.BuildSenderEndpoint)
-				.As<ISendToEndpoints>()
-				.SingleInstance();
+        public virtual EndpointWireup RetryAtLeast(int times)
+        {
+            this.maxRetries = times;
+            return this;
+        }
+        public virtual EndpointWireup IgnoreTransactions()
+        {
+            this.enlist = false;
+            return this;
+        }
 
-			builder
-				.Register(this.BuildPoisonEndpoint)
-				.Named(PoisonEndpoint, typeof(ISendToEndpoints))
-				.SingleInstance();
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder
+                .Register(this.BuildReceiverEndpoint)
+                .As<IReceiveFromEndpoints>()
+                .SingleInstance();
 
-			builder
-				.Register(this.BuildPoisonMessageHandler)
-				.As<IHandlePoisonMessages>()
-				.SingleInstance()
-				.ExternallyOwned();
-		}
-		protected virtual ISendToEndpoints BuildSenderEndpoint(IComponentContext c)
-		{
-			return new MsmqSenderEndpoint(
-				address => MsmqConnector.OpenSend(new MsmqAddress(address.ToString()), this.enlist),
-				c.Resolve<ISerializeMessages>());
-		}
-		protected virtual IReceiveFromEndpoints BuildReceiverEndpoint(IComponentContext c)
-		{
-			return new MsmqReceiverEndpoint(
-				MsmqConnector.OpenReceive(new MsmqAddress(this.receiverAddress), this.enlist),
-				MsmqConnector.OpenSend(new MsmqAddress(this.poisonAddress), this.enlist),
-				c.Resolve<ISerializeMessages>());
-		}
-		protected virtual ISendToEndpoints BuildPoisonEndpoint(IComponentContext c)
-		{
-			// ignore address provided, always send to the configured poison address
-			var address = new MsmqAddress(this.poisonAddress);
-			return new MsmqSenderEndpoint(
-				addr => MsmqConnector.OpenSend(address, PoisonEndpointEnlist),
-				c.Resolve<ISerializeMessages>());
-		}
-		protected virtual IHandlePoisonMessages BuildPoisonMessageHandler(IComponentContext c)
-		{
-			if (!this.enlist)
-				return new NonTransactionalPoisonMessageHandler();
+            builder
+                .Register(this.BuildSenderEndpoint)
+                .As<ISendToEndpoints>()
+                .SingleInstance();
 
-			return new PoisonMessageHandler(
-                c.ResolveNamed<ISendToEndpoints>(PoisonEndpoint, new Parameter[0]),new Uri(this.poisonAddress), this.maxRetries);
-		}
-	}
+            builder
+                .Register(this.BuildPoisonEndpoint)
+                .Named(PoisonEndpoint, typeof(ISendToEndpoints))
+                .SingleInstance();
+
+            builder
+                .Register(this.BuildPoisonMessageHandler)
+                .As<IHandlePoisonMessages>()
+                .SingleInstance()
+                .ExternallyOwned();
+        }
+        protected virtual ISendToEndpoints BuildSenderEndpoint(IComponentContext c)
+        {
+            return new MsmqSenderEndpoint(
+                address => MsmqConnector.OpenSend(new MsmqAddress(address.ToString()), this.enlist),
+                c.Resolve<ISerializeMessages>());
+        }
+        protected virtual IReceiveFromEndpoints BuildReceiverEndpoint(IComponentContext c)
+        {
+            return new MsmqReceiverEndpoint(
+                MsmqConnector.OpenReceive(new MsmqAddress(this.receiverAddress), this.enlist),
+                MsmqConnector.OpenSend(new MsmqAddress(this.poisonAddress), this.enlist),
+                c.Resolve<ISerializeMessages>());
+        }
+        protected virtual ISendToEndpoints BuildPoisonEndpoint(IComponentContext c)
+        {
+            // ignore address provided, always send to the configured poison address
+            var address = new MsmqAddress(this.poisonAddress);
+            return new MsmqSenderEndpoint(
+                addr => MsmqConnector.OpenSend(address, PoisonEndpointEnlist),
+                c.Resolve<ISerializeMessages>());
+        }
+        protected virtual IHandlePoisonMessages BuildPoisonMessageHandler(IComponentContext c)
+        {
+            if (!this.enlist)
+                return new NonTransactionalPoisonMessageHandler();
+
+            return new PoisonMessageHandler(
+                c.ResolveNamed<ISendToEndpoints>(PoisonEndpoint, new Parameter[0]), new Uri(this.poisonAddress), this.maxRetries,this.postHandlingAction);
+        }
+    }
 }
