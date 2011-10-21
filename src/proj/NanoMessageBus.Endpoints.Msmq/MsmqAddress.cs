40c53e17de8761d258cf9f91da8e3ab1490b6556
Iulian Margarintescu <etishor@gmail.com>
2011-10-21 12:32:46 +0300

    Fix MsmqFormat

--- src/proj/NanoMessageBus.Endpoints.Msmq/MsmqAddress.cs
+++ src/proj/NanoMessageBus.Endpoints.Msmq/MsmqAddress.cs
@@ -1,55 +1,55 @@
 namespace NanoMessageBus.Endpoints
 {
-	using System;
-	using System.Text.RegularExpressions;
+    using System;
+    using System.Text.RegularExpressions;
 
-	public class MsmqAddress
-	{
-		private const string LocalHost = ".";
-		private const string MsmqFormat = @"{0}\PRIVATE$\{1}";
-		private const string CanonicalFormat = @"msmq://{0}/{1}";
-		private const string Pattern = @"^((msmq\://)?([A-Za-z0-9-_.]+)/)?([A-Za-z0-9-_.]+)(/)?$";
-		private const int HostNameCapture = 3;
-		private const int QueueNameCapture = 4;
-		private static readonly Regex AddressFormatRegex = new Regex(Pattern, RegexOptions.Compiled);
+    public class MsmqAddress
+    {
+        private const string LocalHost = ".";
+        private const string MsmqFormat = @"FormatName:Direct=OS:{0}\PRIVATE$\{1}";
+        private const string CanonicalFormat = @"msmq://{0}/{1}";
+        private const string Pattern = @"^((msmq\://)?([A-Za-z0-9-_.]+)/)?([A-Za-z0-9-_.]+)(/)?$";
+        private const int HostNameCapture = 3;
+        private const int QueueNameCapture = 4;
+        private static readonly Regex AddressFormatRegex = new Regex(Pattern, RegexOptions.Compiled);
 
-		private readonly Uri canonical;
-		private readonly string proprietary;
+        private readonly Uri canonical;
+        private readonly string proprietary;
 
-		public MsmqAddress(string address)
-		{
-			if (string.IsNullOrEmpty(address))
-				throw new ArgumentException(Diagnostics.InvalidAddress.FormatWith(address), "address");
+        public MsmqAddress(string address)
+        {
+            if (string.IsNullOrEmpty(address))
+                throw new ArgumentException(Diagnostics.InvalidAddress.FormatWith(address), "address");
 
-			var match = AddressFormatRegex.Match(address);
-			if (!match.Success)
-				throw new ArgumentException(Diagnostics.InvalidAddress.FormatWith(address), "address");
+            var match = AddressFormatRegex.Match(address);
+            if (!match.Success)
+                throw new ArgumentException(Diagnostics.InvalidAddress.FormatWith(address), "address");
 
-			var machineName = GetMachineName(match.Groups[HostNameCapture].Value);
-			var queueName = match.Groups[QueueNameCapture].Value;
+            var machineName = GetMachineName(match.Groups[HostNameCapture].Value);
+            var queueName = match.Groups[QueueNameCapture].Value;
 
-			this.proprietary = MsmqFormat.FormatWith(machineName, queueName);
-			this.canonical = new Uri(CanonicalFormat.FormatWith(machineName, queueName));
-		}
-		private static string GetMachineName(string value)
-		{
-			value = (value ?? string.Empty).Trim();
-			value = value.Length == 0 || value == LocalHost ? Environment.MachineName : value;
-			return value.ToLowerInvariant();
-		}
+            this.proprietary = MsmqFormat.FormatWith(machineName, queueName);
+            this.canonical = new Uri(CanonicalFormat.FormatWith(machineName, queueName));
+        }
+        private static string GetMachineName(string value)
+        {
+            value = (value ?? string.Empty).Trim();
+            value = value.Length == 0 || value == LocalHost ? Environment.MachineName : value;
+            return value.ToLowerInvariant();
+        }
 
-		public virtual Uri Canonical
-		{
-			get { return this.canonical; }
-		}
-		public virtual string Proprietary
-		{
-			get { return this.proprietary; }
-		}
+        public virtual Uri Canonical
+        {
+            get { return this.canonical; }
+        }
+        public virtual string Proprietary
+        {
+            get { return this.proprietary; }
+        }
 
-		public override string ToString()
-		{
-			return this.proprietary;
-		}
-	}
+        public override string ToString()
+        {
+            return this.proprietary;
+        }
+    }
 }
